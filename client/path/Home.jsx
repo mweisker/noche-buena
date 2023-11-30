c6e99060c6f5e8aa75ec3113e07a7f1555ee0cc7
Matthew Weisker <mweisker27@gmail.com>
2023-11-29 16:05:26 -0800

    Attempting to attatch pictures

--- client/path/Home.jsx
+++ client/path/Home.jsx
@@ -4,6 +4,10 @@ import { useNavigate } from 'react-router-dom';
 
 import Component from '../components/Component.jsx';
 
+// import asdf from '../img/'
+
+import image from '../img/room-background.png';
+
 export default function Home() {
 
   const navigate = useNavigate();
@@ -12,6 +16,8 @@ export default function Home() {
     <div className='home'>
       <h1>Home Page</h1>
       <Component/>
+      <img src={image} alt='nothing'></img>
+      <div className='gif'></div>
     </div>
   )
 }
